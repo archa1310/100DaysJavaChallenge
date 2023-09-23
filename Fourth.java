7310cd2e87717c0576ba8ba4dedf8fc17f6eae19
archa1310 <71759798+archa1310@users.noreply.github.com>
2023-09-24 00:28:55 +0530

    Add files via upload

---
+++ Fourth.java
@@ -0,0 +1,16 @@
+public class Fourth {
+    
+    public static void main(String[] args) {
+        int a = 'J';
+        int b = 'A';
+        int c = 'V';
+        int d = 'A';
+        System.out.println(a+" "+b+" "+c+" "+d);
+        char e = 74;
+        char f = 65;
+        char g = 86;
+        char h = 65;
+        System.out.println(e+" "+f+" "+g+" "+h);
+    }
+    }
+
